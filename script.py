9a256b82640debd7916b16225ced80aaa489e7c9
Erfan Golchini <erfangolchini@esentris-MacBook-Pro.local>
2020-11-10 20:37:15 +0100

    Initial Commit

--- script.py
+++ script.py
@@ -6,5 +6,5 @@ with open('data.json') as json_file:
     for p in data['creation']:
         creation_name = p['title']
         for creator in re.split(', |und',p['creator_name']):
-            text_file.write('text: Wer hat "'+ creation_name+ '" erstellt?\tlabel: '+ creator+"\t\n")
+            text_file.write('text: Wer hat "'+ creation_name+ '" erstellt?\tlabels: '+ creator+"\t\n")
 
